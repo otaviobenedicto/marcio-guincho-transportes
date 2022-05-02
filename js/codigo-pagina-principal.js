b11880dfde7c192124466842b3890b22cfa9d9cd
Otavio Benedicto <otavio.bene@outlook.com>
2022-05-02 17:50:07 -0300

    atualização dia 02/05/2022

--- js/codigo-pagina-principal.js
+++ js/codigo-pagina-principal.js
@@ -58,7 +58,7 @@ function activeMenu() {
 
     let len = sec.length;
 
-    while (--len && window.scrollY + 1100 < sec[len].offsetTop) { }
+    while (--len && window.scrollY + 1000 < sec[len].offsetTop) { }
 
     li.forEach(ltx => ltx.classList.remove("activeMenu"));
 
@@ -83,3 +83,49 @@ window.addEventListener("scroll", () => {
     lastScrollY = window.scrollY
 });
 
+//Carrouse Slide Comentary
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+			center: true,
+	    loop: false,
+	    autoplay: false,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
